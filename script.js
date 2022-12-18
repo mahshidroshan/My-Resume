63b8874eead2686e6c42e8e926eec7e8db9362c5
Mahshid <120680859+mahshidroshan@users.noreply.github.com>
2022-12-18 15:28:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,6 @@ $(".animated-progress span").each(function () {
 
 //Contact Form
 document.querySelector("#contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
   e.target.elements.name.value = "";
   e.target.elements.email.value = "";
   e.target.elements.message.value = "";
